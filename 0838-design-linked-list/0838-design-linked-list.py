ffe19b6fa8fec23b8c4aac43a9e8f7c388d7fef1
tobechukwujs <tobechukwuegboh@gmail.com>
2025-12-29 08:05:15 +0100

    Time: 131 ms (69.83%), Space: 14.7 MB (8.62%) - LeetHub

---
+++ 0838-design-linked-list/0838-design-linked-list.py
@@ -0,0 +1,63 @@
+class MyLinkedList(object):
+
+    def __init__(self):
+        self.left = ListNode(0)
+        self.right = ListNode(0)
+        self.left.next = self.right
+        self.right.prev = self.left
+                
+
+    def get(self, index):
+
+        cur = self.left.next
+        while cur and index > 0:
+            cur = cur.next
+            index -= 1
+        if cur and cur != self.right and index == 0:
+            return cur.val
+        return -1        
+
+    def addAtHead(self, val):
+        node, next, prev = ListNode(val), self.left.next, self.left
+        prev.next = node
+        next.prev = node
+        node.next = next
+        node.prev = prev
+
+    def addAtTail(self, val):
+        node, next, prev = ListNode(val), self.right, self.right.prev
+        prev.next = node
+        next.prev = node
+        node.next = next
+        node.prev = prev
+
+        
+        
+
+    def addAtIndex(self, index, val):
+        cur = self.left.next
+        while cur and index > 0:
+            cur = cur.next
+            index -= 1
+        if cur and index ==0:
+            node, next, prev = ListNode(val), cur, cur.prev
+            prev.next = node
+            next.prev = node
+            node.next = next
+            node.prev = prev    
+        
+
+        
+
+    def deleteAtIndex(self, index):
+        cur = self.left.next
+        while cur and index > 0:
+            cur = cur.next
+            index -= 1
+        if cur and cur != self.right and index ==0:
+            next,prev = cur.next, cur.prev
+            next.prev = prev
+            prev.next = next
+
+        
+        
