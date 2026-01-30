1b41097c2b43ac15422282b4ffbf2f509990e2e1
Tobechukwu Praise-God Egboh <tobechukwuegboh@gmail.com>
2026-01-30 08:27:11 +0100

    Time: 3 ms (55.48%), Space: 16.6 MB (73.42%) - LeetHub

---
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        if not root:
+            return TreeNode(val)
+
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        elif val > root.val:
+
+            root.right = self.insertIntoBST(root.right, val)
+        
+        return root            
+        
