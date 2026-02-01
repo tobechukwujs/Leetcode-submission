8cb5eff366898e40a18305645e0cf8a36d143dca
Tobechukwu Praise-God Egboh <tobechukwuegboh@gmail.com>
2026-02-01 23:56:02 +0100

    Time: 68 ms (6.91%), Space: 20.5 MB (43.27%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):    
+        while root:
+            if p.val < root.val and q.val < root.val:
+                root = root.left
+            elif p.val > root.val and q.val > root.val:
+                root = root.right
+            else:
+                return root
+            
+        return None
+
+
+        
