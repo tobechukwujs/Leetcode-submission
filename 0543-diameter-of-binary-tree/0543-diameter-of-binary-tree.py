24983dcf5fb26f3fc583d9bd974ecae0e9ccd3ab
Tobechukwu Praise-God Egboh <tobechukwuegboh@gmail.com>
2026-02-15 06:16:06 +0100

    Time: 13 ms (46.53%), Space: 26.6 MB (51.66%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        self.res = 0
+        
+        def dfs(curr):
+            if not curr:
+                return 0
+
+            left = dfs(curr.left)
+            right = dfs(curr.right)
+
+            self.res = max(self.res, left + right)
+            return 1 + max(left, right)
+        dfs(root)
+        return self.res
+                
+      
