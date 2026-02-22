0626d641de4458c438ed57810dc1eb03ae197c16
Tobechukwu Praise-God Egboh <tobechukwuegboh@gmail.com>
2026-02-22 08:27:58 +0100

    Time: 71 ms (95.17%), Space: 12.6 MB (93.67%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+        count = defaultdict(int)
+        for char in s:
+            count[char] +=1
+        for i, char in enumerate(s):
+            if count[char] == 1:
+               return i
+        return -1    
