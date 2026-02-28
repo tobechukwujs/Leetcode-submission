bb59fe037c72f3c42ddc99aece05726abf3614d4
Tobechukwu Praise-God Egboh <tobechukwuegboh@gmail.com>
2026-02-28 08:58:02 +0100

    Time: 0 ms (100%), Space: 19.4 MB (81.15%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            # decision to include nums
+            subset.append(nums[i])
+            dfs(i+1)
+            #decision to not include nums
+            subset.pop()
+            dfs(i+1)
+        dfs(0)
+        return res        
