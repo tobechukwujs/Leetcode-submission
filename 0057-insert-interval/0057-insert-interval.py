5740b4c4cf6a452940542d5d4d86cb61c0bdb246
Tobechukwu Praise-God Egboh <tobechukwuegboh@gmail.com>
2026-04-02 11:13:06 +0100

    Time: 0 ms (100%), Space: 21.5 MB (17.9%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.py
@@ -0,0 +1,14 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort()
+        merged = []
+        merged.append(intervals[0])
+        for interval in intervals[1:]:
+            if interval[0] <= merged[-1][1]:
+                merged [-1][1] = max(merged[-1][1], interval[1]) 
+            else:
+                merged.append(interval)   
+        return merged
+        
+        
