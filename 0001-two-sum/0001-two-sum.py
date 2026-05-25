4a4815dea36eb170e84da7b14c54602efd80e518
Tobechukwu Praise-God Egboh <tobechukwuegboh@gmail.com>
2026-05-25 22:46:34 +0100

    Time: 0 ms (100%), Space: 12.9 MB (82.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
-       value = {}
+        value = {}
 
-       for i, num in enumerate(nums):
+        for i, num in enumerate(nums):
             diff = target - num
             if diff in value:
                 return [value[diff], i]
-            
-            value[num] = i    
-            
-
+            value[num] = i      
+              
