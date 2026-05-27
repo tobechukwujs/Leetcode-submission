a7ede617ceebc6e5201d2c788f22c49df5a03bfe
Tobechukwu Praise-God Egboh <tobechukwuegboh@gmail.com>
2026-05-27 19:05:42 +0100

    Time: 15 ms (44.83%), Space: 24 MB (18.31%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,10 +3,7 @@ class Solution:
         res = defaultdict(list)
         for everyWord in strs:
             count = [0] * 26
-            # this code creates multiple 0, 26 to be specific, which we can mimic as a-z
             for c in everyWord:
                 count[ord(c)-ord('a')] +=1
-                 #the code just says when you see a word lik a, put the count in the first 0, like [1,0 0 ...], and  the , #'' is to make python know its a string
             res[tuple(count)].append(everyWord)   
-            #convets the 26list into a tuple, then append every word to that key it ,matches
         return list(res.values())
