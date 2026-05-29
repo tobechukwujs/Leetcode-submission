df156efeb34c14e3204ce02191a58a6e9f6d2914
Tobechukwu Praise-God Egboh <tobechukwuegboh@gmail.com>
2026-05-29 15:53:54 +0100

    Time: 0 ms (100%), Space: 20.7 MB (9.07%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,15 +1,13 @@
-class Solution(object):
-    def search(self, nums, target):
-        left = 0
-        right = len(nums) - 1
-        
-        while left <= right:
-            mid = (left + right) // 2
-
-            if target > nums[mid]:
-                left = mid + 1
-            elif target < nums[mid]:
-                right = mid -1
-            else:
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)-1
+        while l <= r:
+            mid = (l + r)//2
+            if nums[mid] == target:
                 return mid
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1                           
         return -1
