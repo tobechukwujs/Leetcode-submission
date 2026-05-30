b259dd87fb40da04fea5a063a749f58bd2100e10
Tobechukwu Praise-God Egboh <tobechukwuegboh@gmail.com>
2026-05-30 21:44:25 +0100

    Time: 0 ms (100%), Space: 22.2 MB (99.32%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -21,11 +21,3 @@ class MinStack:
     def getMin(self) -> int:
         return self.minStack[-1]
         
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
