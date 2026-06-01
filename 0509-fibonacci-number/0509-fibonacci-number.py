a3086ae7948d052a2c7c2b9b4a678660ba1b7539
Tobechukwu Praise-God Egboh <tobechukwuegboh@gmail.com>
2026-06-01 16:53:41 +0100

    Time: 310 ms (27.29%), Space: 19.1 MB (88.74%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,15 +1,8 @@
-class Solution(object):
-    def fib(self, n):
-        if n <= 1:
-            return n
-        a = 0
-        b = 1
-        for i in range (2, n+1):
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
 
-            curr = a + b
-            a = b
-            b = curr
-        return curr    
-
-
-        
+        return self.fib(n-1) + self.fib(n-2)        
